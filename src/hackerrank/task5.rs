7cffd8477c0ec6e16198c153fa3b28d80efdd501
MaximBovenko034 <maxim.bovenko@e-u.edu.ua>
2026-03-13 14:26:40 +0300

    Practice 5: Apple and Orange

---
+++ src/hackerrank/task5.rs
@@ -0,0 +1,39 @@
+pub fn count_apples_and_oranges(
+    s: i32,
+    t: i32,
+    a: i32,
+    b: i32,
+    apples: Vec<i32>,
+    oranges: Vec<i32>,
+) -> (i32, i32) {
+    let mut apple_count = 0;
+    let mut orange_count = 0;
+
+    for apple in apples {
+        let pos = a + apple;
+        if pos >= s && pos <= t {
+            apple_count += 1;
+        }
+    }
+
+    for orange in oranges {
+        let pos = b + orange;
+        if pos >= s && pos <= t {
+            orange_count += 1;
+        }
+    }
+
+    (apple_count, orange_count)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_count_apples_and_oranges() {
+        let result = count_apples_and_oranges(7, 11, 5, 15, vec![-2, 2, 1], vec![5, -6]);
+
+        assert_eq!(result, (1, 1));
+    }
+}
