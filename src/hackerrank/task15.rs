d4f40ee0f940287221f137c85090b8a38b4cfffc
MaximBovenko034 <maxim.bovenko@e-u.edu.ua>
2026-03-16 14:05:11 +0300

    Practice 15: Drawing Book

---
+++ src/hackerrank/task15.rs
@@ -0,0 +1,16 @@
+pub fn page_count(n: i32, p: i32) -> i32 {
+    let from_front = p / 2;
+    let from_back = (n / 2) - (p / 2);
+
+    from_front.min(from_back)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_page_count() {
+        assert_eq!(page_count(5, 4), 0);
+    }
+}
